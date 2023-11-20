e16062a91da4257f8e1ffe5b106039edd390215e
Alejandro Soto <alexsotog82@gmail.com>
2023-11-20 16:10:52 -0600

    feat: add notFoundException

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
-import {CarsService} from "./cars.service";
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
+import { Car, CarsService } from "./cars.service";
 
 @Controller('cars')
 export class CarsController {
@@ -7,13 +7,23 @@ export class CarsController {
   constructor(private readonly carsService: CarsService) {
   }
   @Get()
-  getAllCars(): Array<string> {
+  getAllCars(): Array<Car> {
     return this.carsService.getAllCars()
   }
 
   @Get(':id')
-  getCarById(@Param('id', ParseIntPipe) id: number): string {
+  getCarById(@Param('id', ParseIntPipe) id: number): Car {
     return this.carsService.findCarById(id);
+  }
+
+  @Post()
+  createCar(@Body() car: Car) {
+    this.carsService.addCar(car)
+  }
+
+  @Patch(':id')
+  updateCar(@Body() car: Car, @Param('id', ParseIntPipe) id: number) {
+    
   }
 
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,14 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
+
+export interface Car {
+  id: number
+  name: string
+  brand: string
+}
 
 @Injectable()
 export class CarsService {
-  private cars = ['Honda', 'Toyota', 'Jeep'];
+  private cars: Array<Car> = [
+    {
+      id: 1,
+      name: 'Fit',
+      brand: 'Honda'
+    },
+    {
+      id: 2,
+      name: 'Rio',
+      brand: 'KIA'
+    },
+    {
+      id: 3,
+      name: 'Beat',
+      brand: 'Chevrolet'
+    }
+  ];
 
-  public getAllCars(): Array<string> {
-    return  this.cars
+  public getAllCars(): Array<Car> {
+    return this.cars
   }
 
-  public findCarById(id: number): string {
-    return  this.cars[id];
+  public findCarById(id: number): Car {
+    const car = this.cars.find(c => c.id === id);
+
+    if(!car) throw new NotFoundException(`Car with id ${id} Not found`);
+
+    return car
+
+  }
+
+
+  public addCar(car: Car) {
+    return this.cars.push(car)
   }
 }
